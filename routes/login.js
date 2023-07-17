86237a2cd4c85da14d649ecf664b5c8ca6a0878d
Kyle Govinder <govinder.kyleandrew@gmail.com>
2023-07-17 09:19:47 +0200

    Level 3: Hashed Passwords

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 router.use(express.urlencoded({ extended: true }));
 
+const md5 = require("md5");
+
 const User = require("../models/User");
 
 router
@@ -13,11 +15,14 @@ router
   })
 
   .post(async (req, res) => {
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
     try {
-      const loginAttempt = await User.findOne({ username: req.body.username });
+      const loginAttempt = await User.findOne({ username: username });
 
       if (loginAttempt) {
-        if (loginAttempt.password === req.body.password) {
+        if (loginAttempt.password === password) {
           res.render("secrets");
         } else {
           res.json({ message: "Username and Password do not match." });
--- routes/register.js
+++ routes/register.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 router.use(express.urlencoded({ extended: true }));
 
+const md5 = require("md5");
+
 const User = require("../models/User");
 
 router
@@ -16,7 +18,7 @@ router
     try {
       const newUser = new User({
         username: req.body.username,
-        password: req.body.password,
+        password: md5(req.body.password),
       });
 
       await newUser.save();
